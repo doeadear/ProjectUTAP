58107169379f240e54466e0d95bfae236a658420
doeadear <32136872+doeadear@users.noreply.github.com>
2017-11-21 10:22:11 -0600

    Added defaults for lot distance and wait time

--- app/src/main/java/uta/utap/LotSettings.java
+++ app/src/main/java/uta/utap/LotSettings.java
@@ -6,8 +6,21 @@ import java.util.List;
  * Created by emiko on 11/20/2017.
  */
 
-public class LotSettings {
-    private float m_MaxLotDistance;
+public class LotSettings
+{
+    private float m_MaxLotDistance; // lot distance in meters TODO miles instead?
     private int m_MaxWaitTime; // wait time in minutes
     private List<Lot> m_PreferredLots;
+
+    private static final float DEFAULT_MAX_LOT_DIST = 500.0f;
+    private static final int DEFAULT_MAX_WAIT_TIME = 10;
+    public LotSettings()
+    {
+    }
+
+    public void setDefaultLotSettings()
+    {
+        m_MaxLotDistance = DEFAULT_MAX_LOT_DIST;
+        m_MaxWaitTime = DEFAULT_MAX_WAIT_TIME;
+    }
 }
