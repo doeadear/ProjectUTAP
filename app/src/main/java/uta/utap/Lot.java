2ba093666ae523568296392c7622602f27e6907b
doeadear <32136872+doeadear@users.noreply.github.com>
2017-11-20 23:29:39 -0600

    Add LotSettings member variables and Lot class

---
+++ app/src/main/java/uta/utap/Lot.java
@@ -0,0 +1,8 @@
+package uta.utap;
+
+/**
+ * Created by emiko on 11/20/2017.
+ */
+
+class Lot {
+}
--- app/src/main/java/uta/utap/LotSettings.java
+++ app/src/main/java/uta/utap/LotSettings.java
@@ -1,8 +1,13 @@
 package uta.utap;
 
+import java.util.List;
+
 /**
  * Created by emiko on 11/20/2017.
  */
 
 public class LotSettings {
+    private float m_MaxLotDistance;
+    private int m_MaxWaitTime; // wait time in minutes
+    private List<Lot> m_PreferredLots;
 }
