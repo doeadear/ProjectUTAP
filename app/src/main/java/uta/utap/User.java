8e73ca1415fdb9c82ee1d64fe2da97b1db75b220
doeadear <32136872+doeadear@users.noreply.github.com>
2017-11-21 23:39:29 -0600

    User accessor methods

--- app/src/main/java/uta/utap/User.java
+++ app/src/main/java/uta/utap/User.java
@@ -10,4 +10,30 @@ public class User {
     private String m_Name;
     private Location m_Destination;
     private UtappSettings m_Settings;
+
+    // Accessor methods
+    public String getName()
+    {
+        return m_Name;
+    }
+
+    public Location getDestination()
+    {
+        return m_Destination;
+    }
+
+    public UtappSettings getUserSettings()
+    {
+        return m_Settings;
+    }
+
+    public void setName(String name)
+    {
+        m_Name = name;
+    }
+
+    public void setDestination(Location destination)
+    {
+        m_Destination = destination;
+    }
 }
