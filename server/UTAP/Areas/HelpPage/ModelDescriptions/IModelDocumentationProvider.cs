8a5a54a9dd51c6601dd1992b2b132b58762ac378
Jenny <whober0521@gmail.com>
2017-11-21 23:10:31 -0600

    Server Application

---
+++ server/UTAP/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace UTAP.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
