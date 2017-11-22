450ef0ef56f2045bcdb2c5091b23dd89b090970d
Jenny <whober0521@gmail.com>
2017-11-21 23:13:44 -0600

    Add DBService

---
+++ server/UTAP/Services/DBService.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace UTAP.Services
+{
+    public class DBService
+    {
+        public string dbHost = "localhost";
+        public string dbUser = "root";
+        public string dbPass = "86750831";
+        public string dbName = "utap";
+
+        public DBService()
+        {
+            string connStr = "server=" + dbHost + ";uid=" + dbUser + ";pwd=" + dbPass + ";database=" + dbName;
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+            }
+        }
+    }
+}
