6b19cb3625df3108509272e49bc76679e8337bff
doeadear <32136872+doeadear@users.noreply.github.com>
2017-11-21 23:25:11 -0600

    Added Lot methods and members
    No set accessor added for location because lot location shouldn't change after initialization in the constructor

--- app/src/main/java/uta/utap/Lot.java
+++ app/src/main/java/uta/utap/Lot.java
@@ -1,8 +1,50 @@
 package uta.utap;
 
+import android.location.Location;
+
 /**
  * Created by emiko on 11/20/2017.
  */
 
-class Lot {
+class Lot
+{
+    public enum Status { UNAVAILABLE, BUSY, AVAILABLE }
+
+    private Location m_Location; // Center of the lot
+    private boolean m_FacultyOnly; // Faculty only lot
+    private Status m_Status;  // Lot status
+
+    // Constructor
+    public Lot(Location location, boolean facultyOnly, Status status)
+    {
+        m_Location = location;
+        m_FacultyOnly = facultyOnly;
+        m_Status = status;
+    }
+
+    // Accessor methods
+    public Location getLocation()
+    {
+        return m_Location;
+    }
+
+    public Status getStatus()
+    {
+        return m_Status;
+    }
+
+    public boolean isFacultyOnly()
+    {
+        return m_FacultyOnly;
+    }
+
+    public void setStatus(Status status)
+    {
+        m_Status = status;
+    }
+
+    public void setFacultyOnly(boolean facultyOnly)
+    {
+        m_FacultyOnly = facultyOnly;
+    }
 }
