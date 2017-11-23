9d792c376a13106ac0a2efe187c6e828c8897003
doeadear <32136872+doeadear@users.noreply.github.com>
2017-11-23 13:27:35 -0600

    Added PasswordUtils class to handle salt and hash

---
+++ app/src/main/java/uta/utap/PasswordUtils.java
@@ -0,0 +1,54 @@
+package uta.utap;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ * Created by emiko on 11/23/2017.
+ */
+
+public class PasswordUtils
+{
+    private static SecureRandom randomGenerator = new SecureRandom();
+
+    public static String generatePasswordHash(String password, byte[] salt)
+    {
+        String passwordHash = "";
+        try
+        {
+            // Use SHA-256
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            // Add a little salt
+            md.update(salt);
+
+            // Get hash bytes
+            byte[] hashBytes = md.digest(password.getBytes());
+
+            // Convert to hex
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 0; i < hashBytes.length; i++)
+            {
+                sb.append(Integer.toString((hashBytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            // Hashed password in hex
+            passwordHash = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return passwordHash;
+    }
+
+    // Generate salt used to generate hash
+    public static byte[] generateSalt() throws NoSuchAlgorithmException {
+        byte[] salt = new byte[32];
+        randomGenerator.nextBytes(salt);
+
+        return salt;
+    }
+}
