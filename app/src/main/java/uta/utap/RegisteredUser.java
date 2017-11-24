dab4d72e32ff323eccacd897db19044756dc16cd
doeadear <32136872+doeadear@users.noreply.github.com>
2017-11-23 19:32:26 -0600

    Added to RegisteredUser constructor and methods

--- app/src/main/java/uta/utap/RegisteredUser.java
+++ app/src/main/java/uta/utap/RegisteredUser.java
@@ -9,8 +9,38 @@ public class RegisteredUser extends User
     private byte[] m_Salt;
     private String m_Password;
 
+    // Constructor
     public RegisteredUser(String name, String password)
     {
         super(name);
+
+        // Save salt to regenerate hash for password verification
+        m_Salt = PasswordUtils.generateSalt();
+        setPassword(password);
+    }
+
+    // Accessor methods
+    public byte[] getSalt()
+    {
+        return m_Salt;
+    }
+
+    public String getPassword()
+    {
+        return m_Password;
+    }
+
+    public void setPassword(String password)
+    {
+        // Only save hashed password
+        m_Password = PasswordUtils.generatePasswordHash(password, m_Salt);
+    }
+
+    // Utility methods
+    public boolean verifyPassword(String password)
+    {
+        String hashPassword = PasswordUtils.generatePasswordHash(password, m_Salt);
+
+        return hashPassword.equals(m_Password);
     }
 }
