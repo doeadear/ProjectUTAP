96b6d8e9d3a47b42d7d9e05722e2725a695d36cc
doeadear <32136872+doeadear@users.noreply.github.com>
2017-11-27 00:09:44 -0600

    Added constructor and accessor to AccountController

--- app/src/main/java/uta/utap/AccountController.java
+++ app/src/main/java/uta/utap/AccountController.java
@@ -6,6 +6,27 @@ package uta.utap;
 
 public class AccountController
 {
+    private static final AccountController accountController = new AccountController();
+    private User m_User;
+
+    // Constructor
+    private AccountController()
+    {
+        // Create new guest user on initialization since user has not logged in
+        m_User = new GuestUser("Guest");
+    }
+
+    public static AccountController getInstance()
+    {
+        return accountController;
+    }
+
+    // Accessor methods
+    public User getUser()
+    {
+        return m_User;
+    }
+
     // Create guest account
     public boolean createAccount(String uid)
     {
