c9c8a036988572e6a7ecefdbf2f38b03ecb8ab31
doeadear <32136872+doeadear@users.noreply.github.com>
2017-11-28 22:59:34 -0600

    More cleanup

--- app/src/main/java/uta/utap/SIgnup.java
+++ app/src/main/java/uta/utap/SIgnup.java
@@ -19,22 +19,6 @@ import android.widget.TextView;
 public class SIgnup extends AppCompatActivity
 {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-
     // UI references.
     private AutoCompleteTextView mUsernameView;
     private EditText mPasswordView;
